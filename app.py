6cab7237f38323c99af1be616042af784c25911f
khanglovetiktok123-eng <khanglovetiktok123@gmail.com>
2026-09-01 01:58:56 +0700

    app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # TELEGRAM BOT SETUP
 TELEGRAM_BOT_TOKEN = "AAFJQV6TSV-mCRjgi5dwP5rVauWNgvoM0GI"
-TELEGRAM_API_URL = f"https://api.telegram.org/bot{t.me/khangxbot}"
+TELEGRAM_API_URL = f"https://api.telegram.org/bot{}"
 
 # Encrypted Credit Info - DO NOT REMOVE
 _Xk9mN3pL5vR8wQ2 = "e3tZWV1bR0dDWVlbWVhYWFlYWF1cW1tbX19fXFxZWkdHQ1lZW1lYWFhZWFldXFtbWV1ZXFxaWkdHQ1lZW1lb"
